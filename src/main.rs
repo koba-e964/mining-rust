9dc9a3c0aa4ef9c52c578ded916d79f2dffd9681
koba-e964 <koba_max@kobas-Air.pohe>
2018-01-06 19:12:27 +0900

    Add benchmark functionality, slight improvement on performance

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ extern crate rand;
 use self::crypto::digest::Digest;
 use self::crypto::sha1::Sha1;
 use rand::distributions::{IndependentSample, Range};
+use std::time::{Instant};
+
 
 const LEN: usize = 18; // the length of suffixes
 const ITERATION: u64 = 1 << 32; // the number of iterations
 
-fn calculate_hash(prefix: &str, suffix: &[u8]) -> String {
+fn calculate_hash(prefix: &str, suffix: &[u8], buffer: &mut [u8; 20]) {
     // create a Sha1 object
     let mut hasher = Sha1::new();
 
@@ -17,7 +19,7 @@ fn calculate_hash(prefix: &str, suffix: &[u8]) -> String {
     hasher.input(suffix);
 
     // read hash digest
-    hasher.result_str()
+    hasher.result(buffer);
 }
 
 fn main() {
@@ -27,27 +29,46 @@ fn main() {
         return;
     }
     let initial_str = &args[1];
-    let mut min_hash = "}".to_string();
+    let mut min_hash = [0; 20];
+    min_hash[1] = 1;
     let mut rng = rand::thread_rng();
 
     let numero = Range::new(48, 58);
-    for _ in 0 .. ITERATION {
-        let mut suffix = [0; LEN];
-        for i in 0 .. LEN {
-            suffix[i] = numero.ind_sample(&mut rng) as u8;
+    for i in 0 .. 16 {
+        let num_iter = (2 * i + 1) * ITERATION / 256;
+        let start = Instant::now();
+        let mut buffer: [u8; 20] = [0; 20];
+        for _ in 0 .. num_iter {
+            let mut suffix = [0; LEN];
+            macro_rules! display {
+                () => ({
+                    print!("SHA1({}{}) = ",
+                           initial_str,
+                           String::from_utf8(suffix.to_vec()).unwrap());
+                    for i in 0 .. 20 {
+                        print!("{:02x}", buffer[i]);
+                    }
+                    println!("");
+                });
+            }
+            for i in 0 .. LEN {
+                suffix[i] = numero.ind_sample(&mut rng) as u8;
+            }
+            calculate_hash(initial_str, &suffix, &mut buffer);
+            if min_hash > buffer {
+                print!("Hash update: ");
+                display!();
+                min_hash = buffer;
+            } else if &buffer[0..7] < &[0, 0, 0, 0, 0, 0, 4] {
+                print!("Jackpot ");
+                display!();
+            }
         }
-        let hash = calculate_hash(initial_str, &suffix);
-        if min_hash > hash {
-            println!("Hash update: SHA1({}{}) = {}",
-                      initial_str,
-                      String::from_utf8(suffix.to_vec()).unwrap(),
-                      hash);
-            min_hash = hash;
-        } else if &hash[0..6] == "000000" {
-            println!("Jackpot SHA1({}{}) = {}",
-                      initial_str,
-                      String::from_utf8(suffix.to_vec()).unwrap(),
-                      hash);
-        }
+        let end = start.elapsed();
+        let end = end.as_secs() as f64 +
+            end.subsec_nanos() as f64 * 1e-9;
+        println!("Round {}: {}sec", i, end);
+        println!(" {} hashes were produced. speed: {} MH/s",
+                 num_iter, num_iter as f64 / end / 1.0e6);
     }
 }
